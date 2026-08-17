42579998924c8c73558b5cb8c49e03b9808549b6
eskdev24 <eskdev24@gmail.com>
2026-08-17 08:45:55 +0000

    fix: enable core library desugaring and update tflite_flutter dependency to version 0.12.1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,25 +17,20 @@ subprojects {
 }
 
 subprojects {
-    project.plugins.withId("org.jetbrains.kotlin.android") {
-        project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
-        }
-    }
+    if (project.name == "app") return@subprojects
+
     project.plugins.withId("com.android.library") {
         project.afterEvaluate {
             val ext = extensions.findByType<com.android.build.gradle.LibraryExtension>()
-            val javaTarget = ext?.compileOptions?.sourceCompatibility
-            if (javaTarget != null) {
-                project.plugins.withId("org.jetbrains.kotlin.android") {
-                    project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-                        compilerOptions.jvmTarget.set(
-                            when (javaTarget) {
-                                JavaVersion.VERSION_1_8 -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
-                                else -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-                            }
-                        )
-                    }
+            val javaTarget = ext?.compileOptions?.sourceCompatibility ?: JavaVersion.VERSION_17
+            val kotlinTarget = when (javaTarget) {
+                JavaVersion.VERSION_1_8 -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
+                JavaVersion.VERSION_11 -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+                else -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+            }
+            project.plugins.withId("org.jetbrains.kotlin.android") {
+                project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+                    compilerOptions.jvmTarget.set(kotlinTarget)
                 }
             }
         }
