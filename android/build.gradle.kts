9f80ec14bdfd9327f90547e1fe78d5425943248a
eskdev24 <eskdev24@gmail.com>
2026-08-17 02:40:15 +0000

    fix: update Kotlin compile options for better compatibility with AGP 9.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,13 +15,29 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
-    if (project.name != "app") {
-        project.evaluationDependsOn(":app")
-    }
     project.plugins.withId("org.jetbrains.kotlin.android") {
         project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+        }
+    }
+    project.plugins.withId("com.android.library") {
+        project.afterEvaluate {
+            val ext = extensions.findByType<com.android.build.gradle.LibraryExtension>()
+            val javaTarget = ext?.compileOptions?.sourceCompatibility
+            if (javaTarget != null) {
+                project.plugins.withId("org.jetbrains.kotlin.android") {
+                    project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+                        compilerOptions.jvmTarget.set(
+                            when (javaTarget) {
+                                JavaVersion.VERSION_1_8 -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
+                                else -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 }
