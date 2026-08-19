b36e09737c124ed0337ad8c0ce05f0ee84af3a7d
eskdev24 <eskdev24@gmail.com>
2026-08-19 02:08:44 +0000

    style: change bottom tab active indicator from green to blue

--- lib/core/theme/color.dart
+++ lib/core/theme/color.dart
@@ -11,7 +11,7 @@ const onPrimaryContainer = Color(0xFFEEEFFF);
 const outline = Color(0xFF8D90A0);
 const outlineVariant = Color(0xFF2D3748);
 
-const secondaryContainer = Color(0xFF00A572);
+const secondaryContainer = Color(0xFF2563EB);
 const onSecondaryContainer = Color(0xFFFFFFFF);
 
 const tertiaryContainer = Color(0xFF7D4CE7);
